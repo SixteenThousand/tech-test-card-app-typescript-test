4ff8f49e35dcf6cf064255a42bfcea671771b3cf
Thomas <thomas16256@gmail.com>
2024-10-25 15:53:59 +0100

    >> add very basic dark mode with toggle button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,40 @@
-import React from "react";
-import NavBar from './components/NavBar'
-import AllEntries from './routes/AllEntries'
-import NewEntry from './routes/NewEntry'
-import EditEntry from './routes/EditEntry'
-import { EntryProvider } from './utilities/globalContext'
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import React, { useState } from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import AllEntries from "./routes/AllEntries";
+import EditEntry from "./routes/EditEntry";
+import NewEntry from "./routes/NewEntry";
+import { EntryProvider } from "./utilities/globalContext";
 
 export default function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  function toggleDarkMode() {
+    setIsDarkMode((currentMode) => {
+      return !currentMode;
+    });
+  }
+
   return (
-    <section>
-  <Router>
-    <EntryProvider>
-    <NavBar></NavBar>
-      <Routes>
-        <Route path="/" element={<AllEntries/>}>
-        </Route>
-        <Route path="create" element={<NewEntry/>}>
-        </Route>
-        <Route path="edit/:id" element={<EditEntry/>}>
-        </Route>
-      </Routes>
-    </EntryProvider>
-    </Router>
-    </section>
-    
+    <div className={isDarkMode ? "dark" : "light"}>
+      <section className="bg-white h-screen w-screen dark:bg-black">
+        <Router>
+          <EntryProvider>
+            <NavBar></NavBar>
+            <Routes>
+              <Route path="/" element={<AllEntries />}></Route>
+              <Route path="create" element={<NewEntry />}></Route>
+              <Route path="edit/:id" element={<EditEntry />}></Route>
+            </Routes>
+          </EntryProvider>
+        </Router>
+        <button
+          id="dark-mode-toggle"
+          onClick={toggleDarkMode}
+          className="bg-slate-200 text-3xl text-black px-1 h-12 w-12 rounded-xl dark:bg-slate-500 dark:text-slate-100"
+        >
+          {isDarkMode ? "☼" : "☽"}
+        </button>
+      </section>
+    </div>
   );
 }
