4b2baa387647083083e2bd623c6c3430736e281e
Thomas <thomas16256@gmail.com>
2024-10-28 18:41:57 +0000

    >> add new entry button to AllEntries

--- frontend/src/routes/AllEntries.tsx
+++ frontend/src/routes/AllEntries.tsx
@@ -20,58 +20,73 @@ export default function AllEntries() {
     );
   }
   return (
-    <section className="grid grid-cols-2 md:grid-cols-4">
-      {entries.map((entry: Entry, index: number) => {
-        return (
-          <div
-            id={entry.id}
-            key={index}
-            className="
+    <>
+      <section className="mb-6 grid grid-cols-2 md:grid-cols-4">
+        {entries.map((entry: Entry, index: number) => {
+          return (
+            <div
+              id={entry.id}
+              key={index}
+              className="
               bg-gray-light dark:bg-gray-dark
               shadow-md shadow-gray-dark dark:shadow-white
               m-3 p-4 rounded
               flex flex-col justify-between
             "
-          >
-            <div
-              className="
+            >
+              <div
+                className="
                 lg:flex lg:flex-row lg:justify-between
                 font-bold text-sm md:text-lg
               "
-            >
-              <h1>{entry.title}</h1>
-              <p className="font-semibold break-words">
-                Scheduled:&nbsp;
-                <time>{new Date(entry.due_at.toString()).toLocaleDateString()}</time>
-              </p>
-            </div>
-            <p className="text-center text-lg font-light md:mt-2 md:mb-4 mt-1 mb-3">{entry.description}</p>
-            <section className="flex items-center justify-around lg:justify-between flex-col md:flex-row pt-2 md:pt-0">
-              <div className="flex justify-center">
-                <button
-                  onClick={() => {
-                    deleteEntry(entry.id as string);
-                  }}
-                  className="m-1 md:m-2 p-1 font-semibold rounded-md bg-red-500 hover:bg-red-700"
-                >
-                  ✖
-                </button>
-                <button
-                  onClick={() => {
-                    navigate(`/edit/${entry.id}`, { replace: true });
-                  }}
-                  className="m-1 md:m-2 p-1 font-semibold rounded-md bg-blue-500 hover:bg-blue-700"
-                >
-                  🖊
-                </button>
+              >
+                <h1>{entry.title}</h1>
+                <p className="font-semibold break-words">
+                  Scheduled:&nbsp;
+                  <time>{new Date(entry.due_at.toString()).toLocaleDateString()}</time>
+                </p>
               </div>
-              <time className="text-right text-sm md:text-lg">
-                {new Date(entry.created_at.toString()).toLocaleDateString()}
-              </time>
-            </section>
-          </div>
-        );
-      })}
-    </section>
+              <p className="text-center text-lg font-light md:mt-2 md:mb-4 mt-1 mb-3">{entry.description}</p>
+              <section className="flex items-center justify-around lg:justify-between flex-col md:flex-row pt-2 md:pt-0">
+                <div className="flex justify-center">
+                  <button
+                    onClick={() => {
+                      deleteEntry(entry.id as string);
+                    }}
+                    className="m-1 md:m-2 p-1 font-semibold rounded-md bg-red-500 hover:bg-red-700"
+                  >
+                    ✖
+                  </button>
+                  <button
+                    onClick={() => {
+                      navigate(`/edit/${entry.id}`, { replace: true });
+                    }}
+                    className="m-1 md:m-2 p-1 font-semibold rounded-md bg-blue-500 hover:bg-blue-700"
+                  >
+                    🖊
+                  </button>
+                </div>
+                <time className="text-right text-sm md:text-lg">
+                  {new Date(entry.created_at.toString()).toLocaleDateString()}
+                </time>
+              </section>
+            </div>
+          );
+        })}
+      </section>
+      <Link
+        className="
+        text-3xl text-white text-center
+        block
+        w-11/12 py-3 mx-auto
+        rounded-md
+        bg-blue-light dark:bg-blue-dark
+        hover:bg-blue-alt dark:hover:bg-blue-alt
+      "
+        to="/create"
+      >
+        +
+      </Link>
+    </>
   );
 }
